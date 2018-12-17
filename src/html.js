84d4f8211ac26eb607515b627343952305b79475
uoon-dev <uoon.dev@gmail.com>
2018-12-18 00:57:50 +0900

    change favicon again

--- src/html.js
+++ src/html.js
@@ -13,7 +13,7 @@ export default class HTML extends React.Component {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="#D0E0D8" />
           <meta name="apple-mobile-web-app-title" content="Lazywill" />
-          <link rel="apple-touch-icon" href="/icons/apple-icon-57x57.png" sizes="57x57" />
+          {/* <link rel="apple-touch-icon" href="/icons/apple-icon-57x57.png" sizes="57x57" />
           <link rel="apple-touch-icon" href="/icons/apple-icon-60x60.png" sizes="60x60" />
           <link rel="apple-touch-icon" href="/icons/apple-icon-72x72.png" sizes="72x72" />
           <link rel="apple-touch-icon" href="/icons/apple-icon-76x76.png" sizes="76x76" />
@@ -24,7 +24,7 @@ export default class HTML extends React.Component {
           <link rel="apple-touch-icon" href="/icons/apple-icon-180x180.png" sizes="180x180" />
           <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" /> */}
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
